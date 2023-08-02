22b188d636d6d57cd5710224ab96897caac6d8d0
Yuri Tokio <yuri@sheild.com.br>
2023-08-02 07:56:14 -0300

    pdf margin

--- scripts.js
+++ scripts.js
@@ -6,7 +6,6 @@ btnGenerate.addEventListener('click', () => {
 
   // Configuração do arquivo final de PDF
   const options = {
-    padding: [0.1, 0.1, 0.1, 0.1],
     filename: "CV_Yuri_Tokio.pdf",
     html2canvas: {scale: 2},
     jsPDF: {unit: "mm", format: "a4", orientation: "portrait"}
